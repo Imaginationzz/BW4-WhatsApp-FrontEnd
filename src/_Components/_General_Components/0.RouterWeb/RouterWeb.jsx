869321622ad1e48cfab98ff3541af32b6ac514e0
RenieJr <reniejr@gmail.com>
2021-03-01 18:09:05 +0100

    folders

--- src/_Components/_General_Components/0.RouterWeb/RouterWeb.jsx
+++ src/_Components/_General_Components/0.RouterWeb/RouterWeb.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+//PERSONAL COMPONENTS IMPORTS
+//PAGES OR NAVBAR(COMPONENTS TO BE DISPLAYED IN ALL PAGES)
+
 //STYLE IMPORTS
 import "./RouterWeb.scss";
 
