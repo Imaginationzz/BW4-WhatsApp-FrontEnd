59785ae667a945b14afcdf2f3eedde5f2d3f4ebb
ArturSmiles <72197892+ArturSmiles@users.noreply.github.com>
2021-03-02 18:18:01 +0100

    Added Profile Edit

--- src/_Components/_General_Components/0.RouterWeb/RouterWeb.jsx
+++ src/_Components/_General_Components/0.RouterWeb/RouterWeb.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 //PERSONAL COMPONENTS IMPORTS
 import LoginPage from "../../_Pages/0.LoginPage/LoginPage";
+import ProfileEdit from "../../_Pages/3.Profile/ProfileEdit";
 
 //STYLE IMPORTS
 import "./RouterWeb.scss";
@@ -12,6 +13,7 @@ export default function RouterWeb() {
     <Router>
       <Switch>
         <Route path="/" exact render={(props) => <LoginPage {...props} />} />
+        <Route path="/ProfileEdit" exact><ProfileEdit/></Route>
       </Switch>
     </Router>
   );
---
+++ src/_Components/_Pages/3.Profile/ProfileEdit.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Image } from "react-bootstrap"
+import { MdEdit } from "react-icons/md"
+import{IoArrowBack} from "react-icons/io5"
+import "./ProfileEdit.css"
+
+function ProfileEdit() {
+    
+    return (
+        <>
+            <div className="Header">
+                <div className="HeaderContent">
+                <div className="BackIconDiv">
+                <IoArrowBack className="BackIcon"/>
+                </div>
+                <div className="HeaderText">
+                    Profile
+                </div>
+                </div>
+            </div>
+            <div className="EditPage">
+                <div className="ProfilePicDiv">
+                    <Image className="ProfilePic" src="https://via.placeholder.com/200" roundedCircle />
+                </div>
+                <div className="NameDiv">
+                    <div className="NameHeader">
+                        <p>Your Name</p>
+                    </div>
+                    <div className="NameContent">
+                        <div className="Name">
+                            <p>Name Placeholder</p>
+                        </div>
+                        <div className="EditIcon">
+                            <MdEdit className="EditIcon" />
+                        </div>
+                    </div>
+                </div>
+                <div className="NameInfo">
+                    <span>This is your Username. This Name is Visible to your WhatsApp Contacts.</span>
+                </div>
+                <div className="BioDiv">
+                    <div className="BioHeader">
+                        <p>Info</p>
+                    </div>
+                    <div className="BioContent">
+                        <div className="Bio">
+                            <p>PlaceHolder Bio</p>
+                        </div>
+                        <div className="EditIcon">
+                            <MdEdit className="EditIcon" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ProfileEdit
