ea5ed4a6600221c31bdce4f8903c52b47bcc1e33
zenocoz <federico.soncini@gmail.com>
2021-03-05 13:23:14 +0100

    solved minor bugs

--- src/_Components/_Pages/1.MainPage/MainPage.jsx
+++ src/_Components/_Pages/1.MainPage/MainPage.jsx
@@ -8,35 +8,31 @@ import io from "socket.io-client"
 
 //UTILITIES IMPORTS
 
-import { getAllMembers, createRoom, getRoom, checkRooms } from "./utilities";
+import { getAllMembers, createRoom, getRoom, checkRooms } from "./utilities"
 
 //REDUX ACTIONS
-import { setUserList } from "../../../Redux-Store/_Users/actions";
-import { setChat } from "../../../Redux-Store/Chat/actions";
-import { setSide } from "../../../Redux-Store/SideBar/actions";
+import { setUserList } from "../../../Redux-Store/_Users/actions"
+import { setChat } from "../../../Redux-Store/Chat/actions"
+import { setSide } from "../../../Redux-Store/SideBar/actions"
 
 //PERSONAL COMPONENTS IMPORTS
-import SideBar from "./Sub_Components/SideBar/SideBar";
-import ChatBox from "./Sub_Components/ChatBox/ChatBox";
-import ContactList from "./Sub_Components/SideBar/Sub_Components/ContactList/ContactList";
-import NewGroupChat from "./Sub_Components/SideBar/Sub_Components/NewGroupChat/NewGroupChat";
-import Archived from "./Sub_Components/SideBar/Sub_Components/Archived/Archived";
-import StarMsg from "./Sub_Components/SideBar/Sub_Components/StarMsg/StarMsg";
-import Settings from "./Sub_Components/SideBar/Sub_Components/Settings/Settings";
-import Notifications from "./Sub_Components/SideBar/Sub_Components/Settings/Sub_Components/Notifications/Notifications";
-import ProfileEdit from "./Sub_Components/SideBar/Sub_Components/3.Profile/Profile";
-import ThemeModal from "./Sub_Components/Modal/ThemeModal";
-import GroupInfo from "./Sub_Components/SideBar/Sub_Components/NewGroupChat/Sub_Components/GroupInfo/GroupInfo";
+import SideBar from "./Sub_Components/SideBar/SideBar"
+import ChatBox from "./Sub_Components/ChatBox/ChatBox"
+import ContactList from "./Sub_Components/SideBar/Sub_Components/ContactList/ContactList"
+import NewGroupChat from "./Sub_Components/SideBar/Sub_Components/NewGroupChat/NewGroupChat"
+import Archived from "./Sub_Components/SideBar/Sub_Components/Archived/Archived"
+import StarMsg from "./Sub_Components/SideBar/Sub_Components/StarMsg/StarMsg"
+import Settings from "./Sub_Components/SideBar/Sub_Components/Settings/Settings"
+import Notifications from "./Sub_Components/SideBar/Sub_Components/Settings/Sub_Components/Notifications/Notifications"
+import ProfileEdit from "./Sub_Components/SideBar/Sub_Components/3.Profile/Profile"
+import ThemeModal from "./Sub_Components/Modal/ThemeModal"
+import GroupInfo from "./Sub_Components/SideBar/Sub_Components/NewGroupChat/Sub_Components/GroupInfo/GroupInfo"
 
 //BOOTSTRAP IMPORTS
 import { Row, Col, Alert } from "react-bootstrap"
 
 //STYLE IMPORTS
 import "./MainPage.scss"
-import ProfileEdit from "./Sub_Components/SideBar/Sub_Components/3.Profile/Profile"
-
-
-
 
 const connOpt = {
   transports: ["websocket", "polling"],
@@ -47,21 +43,21 @@ let socket = io(process.env.REACT_APP_URL_DEV, connOpt)
 export default function MainPage(props) {
   //STATES
 
-  const [allowed, setAllowed] = useState(false);
-  const [sideBar, setSideBar] = useState("sidebar");
-  const [messages, setMessages] = useState([]);
-  const [message, setMessage] = useState("");
-  const [modal, setModal] = useState(false);
-  const [groupMembers, setGroupMembers] = useState([]);
-  const [groupName, setGroupName] = useState("");
-  
+  const [allowed, setAllowed] = useState(false)
+  const [sideBar, setSideBar] = useState("sidebar")
+  const [messages, setMessages] = useState([])
+  const [message, setMessage] = useState("")
+  const [modal, setModal] = useState(false)
+  const [groupMembers, setGroupMembers] = useState([])
+  const [groupName, setGroupName] = useState("")
+
   //REDUX STATES
   const userState = useSelector((state) => state.userState)
   const tokenState = useSelector((state) => state.tokenState)
   const chatState = useSelector((state) => state.chatState)
-  
+
   //REDUX DISPATCH
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   useEffect(() => {
     ;(async () => {
@@ -69,8 +65,8 @@ export default function MainPage(props) {
         setAllowed(true)
         const allMembers = await getAllMembers(
           tokenState.access_token.access_token
-        );
-        dispatch(setUserList(allMembers));
+        )
+        dispatch(setUserList(allMembers))
       } else {
         setAllowed(false)
         setTimeout(() => {
@@ -80,58 +76,57 @@ export default function MainPage(props) {
     })()
     socket.on("message", (msg) =>
       setMessages((messages) => messages.concat(msg))
-
-    );
-  }, []);
+    )
+  }, [])
 
   useEffect(() => {
-    (async () => {
+    ;(async () => {
       if (chatState.current_chat) {
-        setMessages([]);
+        setMessages([])
         socket.emit("joinRoom", {
           roomId: chatState.current_chat._id,
           username: userState.user._id,
-        });
-        const room = await getRoom(chatState.current_chat._id);
-        setMessages(room.messages);
+        })
+        const room = await getRoom(chatState.current_chat._id)
+        setMessages(room.messages)
       }
-    })();
-  }, [chatState.current_chat]);
+    })()
+  }, [chatState.current_chat])
 
   const joinRoom = async (user, isPrivate, groupPic, roomName) => {
-    let usersId;
-    let roomPic;
+    let usersId
+    let roomPic
     if (isPrivate) {
-      usersId = [{ user }, { user: userState.user }];
-      roomPic = user.picture;
+      usersId = [{ user }, { user: userState.user }]
+      roomPic = user.picture
     } else {
       usersId = user.map((user) => {
-        return { user: user };
-      });
-      groupPic ? (roomPic = groupPic) : (roomPic = null);
+        return { user: user }
+      })
+      groupPic ? (roomPic = groupPic) : (roomPic = null)
     }
     // console.log(usersId);
-    const isOk = await checkRooms(usersId);
+    const isOk = await checkRooms(usersId)
     // console.log(isOk);
-    let room;
+    let room
     if (isOk.length > 0) {
-      room = isOk[0];
+      room = isOk[0]
     } else {
       isPrivate
         ? (room = await createRoom(user.username, usersId, roomPic))
-        : (room = await createRoom(roomName, usersId, roomPic));
+        : (room = await createRoom(roomName, usersId, roomPic))
     }
     // await setChat.current_chat(room);
-    dispatch(setChat(room));
-    dispatch(setSide("sidebar"));
-  };
+    dispatch(setChat(room))
+    dispatch(setSide("sidebar"))
+  }
 
   const handleMessage = async (e) => {
     if (e.keyCode === 13 || e.key === "Enter") {
-      setMessage("");
-      socket.emit("chat", { message, roomId: chatState.current_chat._id });
+      setMessage("")
+      socket.emit("chat", { message, roomId: chatState.current_chat._id })
     } else {
-        setMessage(e.currentTarget.value);
+      setMessage(e.currentTarget.value)
     }
   }
 
@@ -145,7 +140,10 @@ export default function MainPage(props) {
 
       //b64.string will show the stringified
 
-      socket.emit("image", { roomId: currentChat._id, image: reader.result })
+      socket.emit("image", {
+        roomId: chatState.current_chat._id,
+        image: reader.result,
+      })
     })
     reader.readAsDataURL(data)
   }
@@ -154,8 +152,8 @@ export default function MainPage(props) {
     socket.emit("leaveRoom", {
       roomId,
       userId: userState.user._id,
-    });
-  };
+    })
+  }
 
   return (
     <div id="main-page">
@@ -192,13 +190,11 @@ export default function MainPage(props) {
             groupName={groupName}
             setGroupName={setGroupName}
           />
-
         </Col>
         <Col xs={12} md={8}>
           <ChatBox
             functions={handleMessage}
             inputMsg={message}
-
             handleImage={handleImage}
             messages={messages}
             setMessage={setMessage}
